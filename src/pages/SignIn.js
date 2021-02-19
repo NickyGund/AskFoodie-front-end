e88a7fad657cb58ba3ee40abb2ea2c75b3672c85
Steve <pickleman20@hotmail.com>
2021-02-19 02:15:15 -0500

    setup login page UI

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,19 +1,125 @@
+// https://reactnative.dev/docs/components-and-apis
 import React from 'react';
-import { View, SafeAreaView, Dimensions, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, PixelRatio, Dimensions, StyleSheet, TouchableOpacity, Text, ImageBackground } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
-const { width, height } = Dimensions.get('window');
-const cross = Math.sqrt(width * width + height * height);
+function get_font_size(size) {
+  return size / PixelRatio.getFontScale();
+};
 
-export default (props) => {
+const styles = StyleSheet.create({
+  background_image: {
+    flexGrow: 1,
+    justifyContent: "flex-start",
+    width: "100%",
+    resizeMode: "cover",
+  },
+  container: {
+    flexGrow: 1,
+    flexDirection: "column",
+  },
+  menu_container: {
+    width: "100%",
+    padding: 4,
+    display: "flex",
+  },
+  title: {
+    fontSize: get_font_size(72),
+    margin: 2,
+    marginTop: "10%",
+    marginBottom: "10%",
+    color: "#66aaffff",
+    fontWeight: "bold",
+    textAlign: "center",
+    textAlignVertical: "bottom",
+  },
+  login_container: {
+    flexDirection: "column",
+    alignItems: "center",
+    paddingTop: 4,
+    paddingRight: 4,
+    paddingBottom: 4-4,
+    paddingLeft: 4,
+    backgroundColor: "#ffffff44",
+  },
+  input: {
+    flexGrow: 1,
+    width: "100%",
+    fontSize: get_font_size(24),
+    padding: 4,
+    marginBottom: 4,
+    backgroundColor: "#ffffffff",
+  },
+  signin: {
+    fontSize: get_font_size(24),
+    textAlign: "center",
+    margin: 12,
+    textAlignVertical: "center",
+    color: "#ffffffff",
+  },
+  bar: {
+    borderColor: "#ffffff44",
+    width: "70%",
+    alignSelf: "center",
+    borderBottomWidth: 1,
+  },
+  signup: {
+    fontSize: get_font_size(20),
+    textAlign: "right",
+    margin: 4,
+    textAlignVertical: "top",
+    color: "#ffffffff",
+  }
+});
+
+const backgroundImage = {
+  uri: "https://i.imgur.com/4SKbUmR.jpg",
+};
+
+function App(component) {
   const toSignUp = () => {
-    props.navigation.navigate('sign up');
+    component.navigation.navigate('sign up');
   };
+
+  var username, password;
+  const username_input_changed = (text) => {
+    username = text
+  }
+  const password_input_changed = (text) => {
+    password = text
+  }
+
+  const toLogin = () => {
+    console.log(username);
+    console.log(password);
+    console.log();
+  };
+
   return (
-    <View style = {{flex:1, alignContent:'center', alignItems:'center', justifyContent:'center'}}>
-      <TouchableOpacity onPress ={toSignUp}>
-        <Text> Go to sign Up</Text>
-      </TouchableOpacity>
-      
+    <View style={styles.container}>
+      <ImageBackground source = {backgroundImage} style = {styles.background_image}>
+        <View style = {styles.menu_container}>
+          <Text adjustsFontSizeToFit style = {styles.title}>
+            Foodie
+          </Text>
+          <View style = {styles.login_container}>
+            <TextInput onChangeText = {username_input_changed} style = {styles.input} placeholder = "Username" textContentType = "username" />
+            <TextInput onChangeText = {password_input_changed} style = {styles.input} placeholder = "Password" textContentType = "password" secureTextEntry />
+          </View>
+          <TouchableOpacity>
+            <Text style = {styles.signin} onPress = {toLogin}>
+              Login
+            </Text>
+          </TouchableOpacity>
+          <View style = {styles.bar} />
+          <TouchableOpacity onPress = {toSignUp}>
+            <Text style = {styles.signup}>
+              Register
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
+export default App;
