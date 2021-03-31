efb6d4e5da6d7e14bc89e1b36282fc39fccc455d
NickyGund <neg62@scarletmail.rutgers.edu>
2021-03-31 17:28:40 -0400

    sucessfully grabs userName from authContext

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,14 @@
 import React, { useContext, Component, useEffect, useState} from 'react';
-import { StyleSheet,Text,ScrollView,SafeAreaView, TouchableOpacity} from 'react-native';
+import {StyleSheet,Text,ScrollView,SafeAreaView, TouchableOpacity, View} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {AuthContext, AuthProvider } from '../context';
+import useWindowDimensions from 'react-native/Libraries/Utilities/useWindowDimensions';
 
 export default (props) => {
- const authContext = useContext(AuthContext);
- var [userName, setUserName] = useState("");
+const width = useWindowDimensions.width;
+const height = useWindowDimensions.height;
+const authContext = useContext(AuthContext);
+var [userName, setUserName] = useState("");
 
     useEffect(() =>{
 
@@ -30,30 +33,38 @@ export default (props) => {
         }
     }
 
+    const styles = StyleSheet.create({
+        screen: {
+            flex: 1,
+        },
+       
+        backButton: {
+            width: 50,
+            height: 50,
+            justifyContent: 'center',
+            backgroundColor : 'pink',
+
+        },
+ 
+    });
+
+
     return (
         <SafeAreaView>
             <ScrollView>
+                <View style = {styles.screen}>
                 <Text>{userName}</Text>
-                <TouchableOpacity 
+                </View>
+                <View style = {styles.screen}>
+                <TouchableOpacity style = {styles.backButton}
                 onPress = {goHome}>
                     <Text>Back</Text>
                 </TouchableOpacity>
+                </View>
             </ScrollView>
         </SafeAreaView>
     )
     
-
-    const styles = StyleSheet.create({
-
-
-
-
-
-
-
-
-
-    })
 
 
 
