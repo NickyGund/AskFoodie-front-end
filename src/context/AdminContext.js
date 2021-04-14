0b2b64c8a64470f04cf3cabb739797bf69baf414
bplotnick0 <bplotnick0@gmail.com>
2021-04-14 11:32:24 -0400

    actual results from database appear on admin page

--- src/context/AdminContext.js
+++ src/context/AdminContext.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 const AdminContext = React.createContext();
 
-const AdminProvider = props => {
-  const [restaurants, setRestaurants] = useState([])
-  const [comments, setComments] = useState([])
-  const [token, setToken] = useState("")
+const AdminProvider = (props) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [token, setToken] = useState("");
 
   // useEffect(() => {
   //   (async () => {
@@ -19,73 +19,74 @@ const AdminProvider = props => {
   //   })();
   // },[]);
 
-  const getComments = async () => {
-    const commentsList = [{id: 1, userName: 'Ben', comment:"letter words starting with “B” and not ending in “E”. Next I get “M” using the assumption that “From” is in the text. “W” is obtained by finding four letter words ending in “Ith” and assuming the most common word out of that list is “With”. Next I get “C” either with the word “Can” or “Which”, assuming one or both is in the text. Then I get “G” by finding all words that end in “in-” and the last letter is one that has not been found yet. This presumably gives all words ending in “ing”. Next “L"}, {id:2, userName: 'joe', comment:"This place is bad!"}]
-    setComments(commentsList)
+  const getComments = async (id) => {
+    const commentsList = [
+      {
+        id: 1,
+        userName: "Ben",
+        comment:
+          "letter words starting with “B” and not ending in “E”. Next I get “M” using the assumption that “From” is in the text. “W” is obtained by finding four letter words ending in “Ith” and assuming the most common word out of that list is “With”. Next I get “C” either with the word “Can” or “Which”, assuming one or both is in the text. Then I get “G” by finding all words that end in “in-” and the last letter is one that has not been found yet. This presumably gives all words ending in “ing”. Next “L",
+      },
+      { id: 2, userName: "joe", comment: "This place is bad!" },
+    ];
+    setComments(commentsList);
+    console.log(id);
     // try {
     //     const res = await axios.get(`http://10.0.0.6:3000/api/places/find`,{
     //         params : {
     //             restaurant: ''
-    //         }, 
+    //         },
     //         headers: {
     //             Authorization: "Bearer " + token,
     //         }
-              
+
     //     })
     // }catch (error) {
     //     throw("Failed to get Comments")
-    // }  
-}
+    // }
+  };
 
-const getRestaurants = async () => {
+  const getRestaurants = async () => {
+    try {
+      const res = await axios.get(
+        `http://192.168.1.11:3000/api/findRestaurant`,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      console.log(res.data.data);
+      setRestaurants(res.data.data);
+    } catch (error) {
+      throw "Failed to get restaurants";
+    }
+  };
 
-  const restaurantsList = [{name: "Mei-Mei Chinese Take-Out"}, 
-                          {name:"Kam Fung Chinese Restaurant"}, 
-                          {name:"Yue Wah Kitchen" },
-                          {name: "Mei-Mei Chinese Take-Out"},
-                          {name: "Mei-Mei Chinese Take-Out"},
-                          {name: "Mei-Mei Chinese Take-Out"},
-                          {name: "Mei-Mei Chinese Take-Out"},
-                          {name: "Mei-Mei Chinese Take-Out"},
-                          {name: "Mei-Mei Chinese Take-Out"},
-                          {name: "Mei-Mei Chinese Take-Out"},
-                          {name: "Mei-Mei Chinese Take-Out"},
-                        ]
-  setRestaurants(restaurantsList)
-    // try {
-    //     const res = await axios.get(`http://10.0.0.6:3000/api/places/find`,{
-    //         headers: {
-    //             Authorization: "Bearer " + token,
-    //         }
-              
-    //     })
-    // }catch (error) {
-    //     throw("Failed to get Comments")
-    // }  
-}
-
-const deleteComment = async (id) => {
-  var filtered = comments.filter(function(value, index, arr){ 
-    return value.id != id;
-  });
-  setComments(filtered)
-}
-  
+  const deleteComment = async (id) => {
+    var filtered = comments.filter(function (value, index, arr) {
+      return value.id != id;
+    });
+    setComments(filtered);
+  };
 
   const state = {
     state: {
-        restaurants,
-        comments
+      restaurants,
+      comments,
     },
     setComments,
     setRestaurants,
     getComments,
     getRestaurants,
-    deleteComment
-   
+    deleteComment,
   };
 
-  return <AdminContext.Provider value={state}>{props.children}</AdminContext.Provider>;
+  return (
+    <AdminContext.Provider value={state}>
+      {props.children}
+    </AdminContext.Provider>
+  );
 };
 
 export { AdminContext, AdminProvider };
--- src/pages/Admin/Comments.js
+++ src/pages/Admin/Comments.js
@@ -1,65 +1,94 @@
-import React, {useState, useContext, useEffect} from 'react';
-import { View, PixelRatio, Dimensions, StyleSheet, ScrollView, Pressable, Text, ImageBackground, useWindowDimensions } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import FoodFilter from '../../components/FoodFilter'
-import Filter from '../../components/filter'
-import { AdminContext, PlacesProvider } from "../../context"
+import React, { useState, useContext, useEffect } from "react";
+import {
+  View,
+  PixelRatio,
+  Dimensions,
+  StyleSheet,
+  ScrollView,
+  Pressable,
+  Text,
+  ImageBackground,
+  useWindowDimensions,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import FoodFilter from "../../components/FoodFilter";
+import Filter from "../../components/filter";
+import { AdminContext, PlacesProvider } from "../../context";
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
-export default ({route}) => {
-    const adminContext = useContext(AdminContext);
-    const width = useWindowDimensions().width;
-    const height = useWindowDimensions().height;
-    const { name } = route.params
+export default ({ route }) => {
+  const adminContext = useContext(AdminContext);
+  const width = useWindowDimensions().width;
+  const height = useWindowDimensions().height;
+  const { name, id } = route.params;
 
-    useEffect(() => {
-        (async () => {
-            adminContext.getComments()
-        })();
-    },[]);
+  useEffect(() => {
+    (async () => {
+      adminContext.getComments(id);
+    })();
+  }, []);
 
-    function get_font_size(size) {
-        return size / PixelRatio.getFontScale();
-      };
-    const toComments = () => {
-        props.navigation.navigate('comments')
-    }
- 
-    return (
-        <View style = {{alignItems:'center', flex:1, width:width}}>
-            <View style = {{flexDirection:'row', justifyContent:'center', marginTop:height*.05, marginBottom: height*.05}}>
-                <Text style = {{fontSize: get_font_size(24)}}> {name} </Text>
-            </View>
-            <View style = {{height:height *.8, }}>
-                <ScrollView style = {{height:height}} showsVerticalScrollIndicator = {false} marginBottom = {'5%'}>
-                    {adminContext.state.comments.map((item,index) => {
-                        return(
-                            <View key = {index} style = {{alignItems:'center'}}>
-                                <View 
-                                     
-                                    style = {{
-                                        borderColor:'grey',
-                                        borderWidth:1, 
-                                        padding:'4%', 
-                                        margin:'5%', 
-                                        width:width*.9, 
-                                        // justifyContent:'center',
-                                        // alignItems:'center'
-                                    }}>
-                                    <Text>User: {item.userName}</Text>
-                                    <Text>Comment: {item.comment}</Text>
-                                </View>
-                                <TouchableOpacity onPress = {() => adminContext.deleteComment(item.id)}>
-                                    <View style = {{backgroundColor:"red", padding:'5%', alignItems:'center'}}>
-                                        <Text>Delete</Text>
-                                    </View>
-                                </TouchableOpacity>
-                            </View>
-                        )
-                        })}
-                </ScrollView>
-            </View>
-        </View>
-    )}
+  function get_font_size(size) {
+    return size / PixelRatio.getFontScale();
+  }
+  const toComments = () => {
+    props.navigation.navigate("comments");
+  };
 
+  return (
+    <View style={{ alignItems: "center", flex: 1, width: width }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          marginTop: height * 0.05,
+          marginBottom: height * 0.05,
+        }}
+      >
+        <Text style={{ fontSize: get_font_size(24) }}> {name} </Text>
+      </View>
+      <View style={{ height: height * 0.8 }}>
+        <ScrollView
+          style={{ height: height }}
+          showsVerticalScrollIndicator={false}
+          marginBottom={"5%"}
+        >
+          {adminContext.state.comments.map((item, index) => {
+            return (
+              <View key={index} style={{ alignItems: "center" }}>
+                <View
+                  style={{
+                    borderColor: "grey",
+                    borderWidth: 1,
+                    padding: "4%",
+                    margin: "5%",
+                    width: width * 0.9,
+                    // justifyContent:'center',
+                    // alignItems:'center'
+                  }}
+                >
+                  <Text>User: {item.userName}</Text>
+                  <Text>Comment: {item.comment}</Text>
+                </View>
+                <TouchableOpacity
+                  onPress={() => adminContext.deleteComment(item.id)}
+                >
+                  <View
+                    style={{
+                      backgroundColor: "red",
+                      padding: "5%",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Text>Delete</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            );
+          })}
+        </ScrollView>
+      </View>
+    </View>
+  );
+};
